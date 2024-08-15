81df5285c8309f5ca2173fab9c62b9ac46acd50c
Alexcious91 <radiopanetiisetso@gmail.com>
2024-08-15 08:30:27 +0200

    Added chapter 04

---
+++ chapter_04/chapter_04.sql
@@ -0,0 +1,16 @@
+-- Listing 4-4: Creating a table to track supervisor salaries
+
+CREATE TABLE supervisor_salaries (
+    town varchar(30),
+    county varchar(30),
+    supervisor varchar(30),
+    start_date date,
+    salary money,
+    benefits money
+);
+
+-- Listing 4-5: Importing salaries data from CSV to three table columns
+
+COPY supervisor_salaries (town, supervisor, salary)
+FROM 'C:\YourDirectory\supervisor_salaries.csv'
+WITH (FORMAT CSV, HEADER);
