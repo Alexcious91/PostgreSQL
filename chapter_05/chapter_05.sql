fea61eea8af1a0588b6914ef8773aef01cd49535
Alexcious91 <radiopanetiisetso@gmail.com>
2024-08-23 21:31:13 +0200

    Minor changes

--- chapter_05/chapter_05.sql
+++ chapter_05/chapter_05.sql
@@ -19,20 +19,20 @@
 -- 	p0010003 + p0010004 AS "Total Black And White"
 -- FROM us_counties_2010
 
--- SELECT 
--- 	geo_name,
--- 	p0010001 AS "Total",
--- 	p0010003  + p0010004 + p0010005 + p0010006 + p0010007 + p0010008 + p0010009 AS "All races",
--- 	(p0010003 + p0010004 + p0010005 + p0010006 + p0010007 + p0010008 + p0010009 ) - p0010001 AS "Difference"
--- FROM us_counties_2010
--- ORDER BY "Difference" DESC
+SELECT 
+	geo_name,
+	p0010001 AS "Total",
+	p0010003  + p0010004 + p0010005 + p0010006 + p0010007 + p0010008 + p0010009 AS "All races",
+	(p0010003 + p0010004 + p0010005 + p0010006 + p0010007 + p0010008 + p0010009 ) - p0010001 AS "Difference"
+FROM us_counties_2010
+ORDER BY "Difference" DESC
 
--- SELECT 
--- 	geo_name,
--- 	state_us_abbreviation AS "st",
--- 	(CAST (p0010006 AS NUMERIC(8, 1)) / p0010001) * 100 AS "pct_asian"
--- FROM us_counties_2010
--- ORDER BY "pct_asian" DESC;
+SELECT 
+	geo_name,
+	state_us_abbreviation AS "st",
+	(CAST (p0010006 AS NUMERIC(8, 1)) / p0010001) * 100 AS "pct_asian"
+FROM us_counties_2010
+ORDER BY "pct_asian" DESC;
 
 -- SELECT sum(p0010001) AS "County Sum",
 -- 	ROUND(AVG(p0010001), 0) AS "Country Average"
